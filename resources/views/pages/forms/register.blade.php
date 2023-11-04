da774ac48462309b66bd44ba0a61b2a21a4dd9a3
Shirloin <110618626+Shirloin@users.noreply.github.com>
2023-11-04 22:17:08 +0700

    authentication: register done

--- resources/views/pages/forms/register.blade.php
+++ resources/views/pages/forms/register.blade.php
@@ -13,14 +13,13 @@
             <div class="hidden lg:block">
                 <div class="w-full h-full flex flex-col justify-center items-end  text-xl">
                     <div class=" h-full flex flex-col justify-center items-center">
-                        <img class="w-96 h-96 object-contain" src="https://images.tokopedia.net/img/content/register_new.png"
-                            alt="Register Image">
+                        <img class="w-96 h-96 object-contain"
+                             src="https://images.tokopedia.net/img/content/register_new.png" alt="Register Image">
                         <h1 class="text-2xl font-bold">Agar tidak terputus minimal seratus</h1>
                     </div>
                 </div>
             </div>
-            <div
-                class="h-full flex flex-col justify-center place-items-center text-xl w-full md:w-96 md:min-h-96 md:p-8 p-4">
+            <div class="h-full flex flex-col justify-center place-items-center text-xl w-full md:w-96 md:min-h-96 md:p-8 p-4">
                 <div
                     class="w-full flex sm:w-96 sm:min-h-96 flex-col justify-start shadow-gray-300 p-8 box-border rounded-md shadow-box gap-8 place-items-center">
                     <div class="w-full flex flex-col justify-center gap-2">
@@ -34,7 +33,7 @@
                         <button
                             class="relative w-full flex justify-center items-center border-solid border-gray-400 border-2 rounded-xl p-2 font-medium text-gray-500 text-base">
                             <img class="absolute w-5 h-5 left-0 m-6 box-border" src="{{ asset('assets/google.png') }}"
-                                alt="">
+                                 alt="">
                             Google
                         </button>
                         <div class="w-full flex justify-center items-center gap-2">
@@ -42,20 +41,13 @@
                             <h1 class="text-sm text-gray-500">or</h1>
                             <hr class="w-full border-t-2 border-gray-300">
                         </div>
-                        <form action="/register" method="POST" class="w-full h-full text-sm flex flex-col gap-1">
-                            @csrf
+                        <form action="" class="w-full h-full text-sm flex flex-col gap-1">
                             <label for="emailInput">Email</label>
                             <input type="text" name="email" id="emailInput" class="input-style">
                             <p class="text-gray-400">Example: email@njpediacx.com</p>
                             <label for="passwordInput">Password</label>
-                            <input type="password" name="password" id="passwordInput" class="input-style">
+                            <input type="password" id="passwordInput" class="input-style">
                             <p class="text-gray-400">Requirements:</p>
-
-                            @if ($errors->any())
-                                <p class="text-red-500 w-full flex justify-center">
-                                    {{$errors->first()}}
-                                </p>
-                            @endif
                             <button class="w-full p-2 my-6 bg-green-500 rounded-lg text-white text-xl font-bold">Register
                             </button>
                         </form>
@@ -65,8 +57,7 @@
         </div>
         <div class="w-full sm:block hidden">
             <div class="w-full flex justify-center items-center">
-                <span class="text-green-500 font-semibold text-center px-20">&copy; DuTiSa, Breaking and Overcoming
-                    Challenges Through Courage, Hardwork, and Persistence</span>
+                <span class="text-green-500 font-semibold text-center px-20">&copy; DuTiSa, Breaking and Overcoming Challenges Through Courage, Hardwork, and Persistence</span>
             </div>
         </div>
     </div>
