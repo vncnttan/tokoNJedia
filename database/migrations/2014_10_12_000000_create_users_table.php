687b76c33fbb786a95b2975d8ce130028328af85
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-11-08 11:42:40 +0700

    fix: Google Login with UUID

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,16 +12,16 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id', 36)->default(DB::raw('(UUID())'))->primary();
+            $table->uuid('id')->default(DB::raw('(UUID())'))->primary();
             $table->string('username')->default("No username");
             $table->string('email')->unique();
             $table->string('password')->nullable();
-            $table->string('phone')->unique()->nullable(true);
+            $table->string('phone')->unique()->nullable();
             $table->float("balance")->default(0);
-            $table->date("dob")->nullable(true);
+            $table->date("dob")->nullable();
             $table->string("gender")->default("");
             $table->string("image")->default("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeMIAx7-Zgl6AdkUXBXZydQPW0EyvuyxAI5w&usqp=CAU");
             $table->string('google_id')->nullable();
@@ -34,7 +34,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
