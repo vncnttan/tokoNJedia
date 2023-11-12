0797e59040a6c9f9d5fa78d0b6227b48a5aa222c
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-11-12 15:57:38 +0700

    feat: Footer responsive

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,31 +1,31 @@
 <div class="w-full p-6 box-border border-t-2 pb-16 sm:block hidden">
-    <div class="flex flex-row px-12 justify-center gap-12">
-        <div class="text-gray-700 flex flex-col gap-2">
+    <div class="flex flex-row md:px-12 justify-center md:gap-12 gap-6">
+        <div class="text-gray-700 flex flex-col gap-2 ">
             <div class="font-bold text-black text-xl">
                 TokoNJedia
             </div>
             <a href="https://www.tokopedia.com/about/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     About TokoNJedia
                 </button>
             </a>
             <a href="https://www.tokopedia.com/perlindungan-kekayaan-intelektual" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Intellectual Property Rights
                 </button>
             </a>
             <a href="https://www.tokopedia.com/careers/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Career
                 </button>
             </a>
             <a href="https://www.tokopedia.com/blog/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Blog
                 </button>
             </a>
             <a href="https://affiliate.tokopedia.com/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     tokoNJedia Affiliate Program
                 </button>
             </a>
@@ -35,17 +35,17 @@
                 Buy
             </div>
             <a href="https://www.tokopedia.com/daftar-halaman/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Top Up & Billing
                 </button>
             </a>
             <a href="https://www.tokopedia.com/tukar-tambah" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Phone exchange
                 </button>
             </a>
             <a href="https://www.tokopedia.com/cod" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Tokonjedia COD
                 </button>
             </a>
@@ -53,17 +53,17 @@
                 Sell
             </div>
             <a href="https://www.tokopedia.com/daftar-halaman/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Seller Education Center
                 </button>
             </a>
             <a href="https://www.tokopedia.com/pinjaman-online/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Mitra Toppers
                 </button>
             </a>
             <a href="https://seller.tokopedia.com/edu/official-store/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Official Store List
                 </button>
             </a>
@@ -74,29 +74,29 @@
                 Help and Support
             </div>
             <a href="https://www.tokopedia.com/help?nref=helpfooter" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     TokoNJedia Care
                 </button>
             </a>
             <a href="https://www.tokopedia.com/about/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Terms and Conditions
                 </button>
             </a>
             <a href="https://www.tokopedia.com/privacy" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Privacy Rules
                 </button>
             </a>
             <a href="https://mitra.tokopedia.com/" class="hover:text-green-600 text-sm" target="_blank">
-                <button>
+                <button class="text-left">
                     Mitra
                 </button>
             </a>
         </div>
-        <div class="flex flex-col justify-center items-center text-green-500 font-semibold text-center px-20">
-            <img src="{{ asset('assets/footer/footer_img.png') }}" class="h-48 w-auto" alt="Footer image">
-            <span class="px-12">Breaking and Overcoming Challenges <br />
+        <div class="flex flex-col justify-center items-center text-green-500 font-semibold text-center md:px-20 px-5">
+            <img src="{{ asset('assets/footer/footer_img.png') }}" class="h-48 w-auto object-cover" alt="Footer image">
+            <span>Breaking and Overcoming Challenges <br />
                  Through Courage, Hardwork, and Persistence</span>
             <h1 class="text-gray-400"> &copy; DuTiSa </h1>
         </div>
