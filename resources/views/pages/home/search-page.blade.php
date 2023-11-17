afd505fd6f6cde3f768d94e0301b52a3868d3313
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-11-17 23:39:52 +0700

    feat: Store page if not found

--- resources/views/pages/home/search-page.blade.php
+++ resources/views/pages/home/search-page.blade.php
@@ -33,7 +33,7 @@
             </button>
         </div>
 
-        <div id="product-result" >
+        <div id="product-result">
             @if(count($products) > 0)
                 <div class="flex flex-col gap-3">
                     <div class="text-sm text-gray-700">
@@ -48,9 +48,38 @@
                         @endforeach
                     </div>
                 </div>
+            @else
+
+                <div class="text-sm text-gray-700">
+                    Searching for
+                    <b>
+                        "{{ $keyword }}"
+                    </b>
+                </div>
+                <div class="w-full h-full rounded-lg shadow-card gap-10 flex flex-row py-6 px-20 my-6 mb-10">
+                    <img alt="Not found" src="{{ url(asset('assets/checkout/not-found.png')) }}"
+                         class="h-32"/>
+                    <div class="flex flex-col justify-evenly">
+                        <h1 class="font-semibold opacity-80 text-2xl">
+                            Oops, we think it hides somewhere
+                        </h1>
+                        <h4>
+                            Try another keyword or check product recommendation below.
+                        </h4>
+                        <a class="w-full" href="/">
+                            <button
+                                class="w-fit h-fit py-3 px-6 rounded-lg text-sm font-semibold text-white bg-green-600">
+                                Go back home
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                <x-recommended-product/>
+
             @endif
         </div>
-        <div id="store-result" >
+        <div id="store-result">
             @if(count($stores) > 0)
 
                 <div class="flex flex-col gap-3">
@@ -66,6 +95,33 @@
                         @endforeach
                     </div>
                 </div>
+            @else
+                <div class="text-sm text-gray-700">
+                    Searching for
+                    <b>
+                        "{{ $keyword }}"
+                    </b>
+                </div>
+                <div class="w-full h-full rounded-lg shadow-card gap-10 flex flex-row py-6 px-20 my-6 mb-10">
+                    <img alt="Not found" src="{{ url(asset('assets/checkout/not-found.png')) }}"
+                         class="h-32"/>
+                    <div class="flex flex-col justify-evenly">
+                        <h1 class="font-semibold opacity-80 text-2xl">
+                            Oops, we think it hides somewhere
+                        </h1>
+                        <h4>
+                            Try another keyword or check product recommendation below.
+                        </h4>
+                        <a class="w-full" href="/">
+                            <button
+                                class="w-fit h-fit py-3 px-6 rounded-lg text-sm font-semibold text-white bg-green-600">
+                                Go back home
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                <x-recommended-product/>
             @endif
         </div>
 
