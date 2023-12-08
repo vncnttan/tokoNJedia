ff2dced89e7fc2e80f36e9f4639588caee977ac8
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-12-09 00:05:04 +0700

    feat: Rating Reply Database

---
+++ app/Http/Controllers/RatingReplyController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RatingReplyController extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_12_08_170329_create_rating_replies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rating_replies', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->uuid('rating_id');
+            $table->string('reply');
+            $table->foreign('rating_id')->references('id')->on('ratings')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rating_replies');
+    }
+};
