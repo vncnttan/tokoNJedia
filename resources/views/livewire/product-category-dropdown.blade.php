dd96f54bb2e2192995c6a62128e61f06d75086a9
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-12-13 21:57:26 +0700

    bug-fix: Category new product ketika pilih masih abu2

--- resources/views/livewire/product-category-dropdown.blade.php
+++ resources/views/livewire/product-category-dropdown.blade.php
@@ -1,9 +1,9 @@
 <div class="w-full" x-data="{ categoryDropdown: false, selectedCategory: '', selectedCategoryName: '{{$category}}', searchQuery: '' }"
     @click.away="categoryDropdown = false">
     <button type="button" x-show="!categoryDropdown"
-        class="w-full h-full p-2 ring-1 ring-gray-300 rounded-md flex justify-between items-center gap-4"
+        class="w-full h-full p-2 ring-1 ring-gray-300 text-black rounded-md flex justify-between items-center gap-4"
         @click.stop="categoryDropdown = !categoryDropdown">
-        <p class="text-sm text-gray-400" x-text="selectedCategoryName"></p>
+        <p class="text-sm text-black" x-text="selectedCategoryName"></p>
         <div class="ml-auto">
             <svg x-show="!categoryDropdown" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                 stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
@@ -15,8 +15,10 @@
             </svg>
         </div>
     </button>
-    <input x-show="categoryDropdown" wire:model.live="search" type="text" placeholder="Search Category..."
-        class="w-full input-style" x-model="searchQuery" @click.away="categoryDropdown = false">
+    <label>
+        <input x-show="categoryDropdown" wire:model.live="search" type="text" placeholder="Search Category..."
+            class="w-full input-style" x-model="searchQuery" @click.away="categoryDropdown = false">
+    </label>
 
     <input type="hidden" name="product_category" :value="selectedCategory" x-bind:value="selectedCategory" wire:model="selectedCategory">
 
