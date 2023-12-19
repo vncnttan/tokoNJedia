0f83113ba56a28e9844f852e235c877b56a5b591
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-12-19 23:51:28 +0700

    feat: button fix

--- resources/views/components/profile/profile-sidebar.blade.php
+++ resources/views/components/profile/profile-sidebar.blade.php
@@ -71,7 +71,7 @@
               action="/logout"
               method="GET">
             <button class="w-full flex items-center gap-3 w-5 h-5" type="submit">
-                <svg class="icon-size" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5"
                      stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"/>
